de11fced406f54cffc9a403ec4095b52971e36a7
Shu Hui <dreamlock0630@gmail.com>
2018-06-15 17:41:12 +1000

    add custome prefix ability to totoro

--- lib/totoro/config.rb
+++ lib/totoro/config.rb
@@ -1,27 +1,28 @@
+# frozen_string_literal: true
+
 module Totoro
   class Config
-    class <<self
-      def data
-        @data ||= Rails.application.config_for(:totoro).with_indifferent_access
-      end
+    def initialize(prefix)
+      @data = Rails.application.config_for(:totoro).with_indifferent_access
+      @data = @data[prefix] if prefix.present?
+    end
 
-      def reset_data
-        @data = nil
-      end
+    def reset_data
+      @data = nil
+    end
 
-      def connect
-        data[:connect]
-      end
+    def connect
+      @data[:connect]
+    end
 
-      def queue(id)
-        name = data[:queue][id][:name]
-        settings = { durable: data[:queue][id][:durable] }
-        [name, settings]
-      end
+    def queue(id)
+      name = data[:queue][id][:name]
+      settings = { durable: data[:queue][id][:durable] }
+      [name, settings]
+    end
 
-      def get_worker(worker_class)
-        ::Worker.const_get(worker_class.to_s.camelize).new
-      end
+    def get_worker(worker_class)
+      ::Worker.const_get(worker_class.to_s.camelize).new
     end
   end
 end
